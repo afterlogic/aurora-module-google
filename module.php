174f5ba353a3e2c826450c92a3e307162feb1100
sash04ek <a.ovcharov@gmail.com>
2017-03-06 08:48:07 +0300

    refactoring namespaces

--- module.php
+++ module.php
@@ -18,9 +18,9 @@
  * @package Modules
  */
 
-namespace Aurora\Modules;
+namespace Aurora\Modules\Google;
 
-class GoogleModule extends \Aurora\System\Module\AbstractModule
+class Module extends \Aurora\System\Module\AbstractModule
 {
 	protected $sService = 'google';
 	
