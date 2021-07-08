a03525b0731dd0debc2c1e09c4196e38a3894019
Evgenii <shestakovevgeniy11@gmail.com>
2021-07-08 15:45:06 +0300

    Fixed bug - border-radius for input

--- vue/components/GoogleAdminSettings.vue
+++ vue/components/GoogleAdminSettings.vue
@@ -14,19 +14,19 @@
           <div class="row q-mb-md">
             <div class="col-2 q-my-sm q-pl-sm required-field" v-t="'OAUTHINTEGRATORWEBCLIENT.LABEL_APP_ID'"></div>
             <div class="col-5">
-              <q-input outlined dense class="bg-white input" v-model="appId"/>
+              <q-input outlined dense bg-color="white" v-model="appId"/>
             </div>
           </div>
           <div class="row q-mb-md">
             <div class="col-2 q-my-sm q-pl-sm required-field" v-t="'OAUTHINTEGRATORWEBCLIENT.LABEL_APP_SECRET'"></div>
             <div class="col-5">
-              <q-input outlined dense class="bg-white input" v-model="appSecret"/>
+              <q-input outlined dense bg-color="white" v-model="appSecret"/>
             </div>
           </div>
           <div class="row q-mb-md">
             <div class="col-2 q-my-sm q-pl-sm required-field" v-t="'GOOGLE.LABEL_API_KEY'"></div>
             <div class="col-5">
-              <q-input outlined dense class="bg-white input" v-model="apiKey"/>
+              <q-input outlined dense bg-color="white" v-model="apiKey"/>
             </div>
           </div>
           <div class="row">
@@ -172,7 +172,5 @@ export default {
 </script>
 
 <style scoped>
-.input {
-  border-radius: 6px;
-}
+
 </style>
