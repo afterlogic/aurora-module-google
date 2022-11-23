f04c8100b98b1c4839123ee9847aef0ac3d73bd4
Vasil Sokolov <vasil@afterlogic.com>
2022-11-23 18:25:40 +0200

    removed code related to the admin panel

--- js/manager.js
+++ js/manager.js
@@ -8,33 +8,10 @@ module.exports = function (oAppData) {
 
 		App = require('%PathToCoreWebclientModule%/js/App.js'),
 
-		Settings = require('modules/%ModuleName%/js/Settings.js'),
-
-		bAdminUser = App.getUserRole() === Enums.UserRole.SuperAdmin
+		Settings = require('modules/%ModuleName%/js/Settings.js')
 	;
 
 	Settings.init(oAppData);
-
-	if (bAdminUser)
-	{
-		return {
-			start: function (ModulesManager) {
-				ModulesManager.run('AdminPanelWebclient', 'registerAdminPanelTab', [
-					function(resolve) {
-						require.ensure(
-							['modules/%ModuleName%/js/views/AdminSettingsView.js'],
-							function() {
-								resolve(require('modules/%ModuleName%/js/views/AdminSettingsView.js'));
-							},
-							"admin-bundle"
-						);
-					},
-					Settings.HashModuleName,
-					TextUtils.i18n('%MODULENAME%/LABEL_SETTINGS_TAB')
-				]);
-			}
-		};
-	}
 
 	if (App.isUserNormalOrTenant() && Settings.EnableModule && _.isArray(Settings.Scopes) && Settings.Scopes.length > 0)
 	{
