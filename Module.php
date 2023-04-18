b0d897d44c2125e1e37267d2f16b350caf424783
Vasil Sokolov <vasil@afterlogic.com>
2023-04-18 11:19:41 +0200

    Refactored module settings

--- Module.php
+++ Module.php
@@ -33,6 +33,15 @@ class Module extends \Aurora\System\Module\AbstractModule
         return parent::Decorator();
     }
 
+    /**
+     *
+     * @return Settings
+     */
+    protected function GetModuleSettings()
+    {
+        return $this->oModuleSettings;
+    }
+
     /***** private functions *****/
     /**
      * Initializes Google Module.
